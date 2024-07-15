496cff58bf9f12a6b3220b91dd8b8b6cc9687164
Arydragons <sunnybgp.icloud.com>
2024-07-15 09:57:20 -0500

    Started on adding Human Player stats to Scouitng Data

--- ScoutingDataAnalyzer/src/application/TempTeam.java
+++ ScoutingDataAnalyzer/src/application/TempTeam.java
@@ -12,8 +12,15 @@ public class TempTeam {
 	private int totalClimb;
 	private int totalTrap;
 	private List<String> listOfComments;
+	private String humPostion;
+	private int humScoSkill;
+	private int humAmpSkill;
+	private int totalHumAmpNotes;
+	private int timesHumAmp;
+	private int timesHumSco;
+	private List<String> listOfHumPostions;
 	
-	TempTeam(int tempTeamNum, int tempTotalCycles, int tempTotalMatchesPlayed, int tempTotalAmp, int tempTotalSpe, int tempTotalClimb, int tempTotalTrap, String comment){
+	TempTeam(int tempTeamNum, int tempTotalCycles, int tempTotalMatchesPlayed, int tempTotalAmp, int tempTotalSpe, int tempTotalClimb, int tempTotalTrap, String comment, String tempHumPostion, int tempHumAmpSkill, int tempHumScoSkill, int tempHumAmpNotes){
 		setTeamNum(tempTeamNum);
 		setTotalCycles(tempTotalCycles);
 		setTotalMatchesPlayed(tempTotalMatchesPlayed);
@@ -23,6 +30,37 @@ public class TempTeam {
 		setTotalClimb(tempTotalClimb);
 		listOfComments = new ArrayList<>();
 		addOnCommentList(comment);
+		setHumPostion(tempHumPostion);
+		if (humPostion.compareTo("Amp") == 0) {
+			timesHumAmp++;
+			setHumAmpSkill(tempHumAmpSkill);
+			setTotalHumAmpNotes(tempHumAmpNotes);
+		}
+		if (humPostion.compareTo("Scoure") == 0) {
+			timesHumSco++;
+			setHumScoSkill(tempHumScoSkill);
+		}
+	}
+	
+	public String setHumPostion(String tempHumanPostion) {
+		return humPostion = tempHumanPostion;
+	}
+	
+	public List<String> setHumPostionList(){
+		listOfHumPostions.add(humPostion);
+		return listOfHumPostions;
+	}
+	
+	public int setHumScoSkill(int tempHumScoSkill) {
+		return humScoSkill = tempHumScoSkill;
+	}
+	
+	public int setHumAmpSkill(int tempHumAmpSkill) {
+		return humAmpSkill = tempHumAmpSkill;
+	}
+	
+	public int setTotalHumAmpNotes(int tempHumAmpNotes) {
+		return totalHumAmpNotes = tempHumAmpNotes;
 	}
 	
 	public int setTeamNum(int tempTeamNum){
@@ -56,6 +94,47 @@ public class TempTeam {
 	public List<String> addOnCommentList(String comment) {
 		listOfComments.add(comment);
 		return listOfComments;
+	}
+	
+	public List<String> addOnHumPostionsList(String postion){
+		listOfHumPostions.add(postion);
+		return listOfHumPostions;
+	}
+	
+	public void clearCommentsList(){
+		listOfComments.clear();
+	}
+	
+	public void clearHumPostionList(){
+		listOfComments.clear();
+	}
+	
+	public String getHumPostion() {
+		return humPostion;
+	}
+	
+	public List<String> getHumanPostionList() {
+		return listOfHumPostions;
+	}
+	
+	public int getHumAmpSkill() {
+		return humAmpSkill;
+	}
+	
+	public int getHumScoSkill() {
+		return humScoSkill;
+	}
+	
+	public int getHumAmpNotes() {
+		return totalHumAmpNotes;
+	}
+	
+	public int getTimesHumAmp() {
+		return timesHumAmp;
+	}
+	
+	public int getTimesHumSco() {
+		return timesHumSco;
 	}
 	
 	public int getTeamNum() {
