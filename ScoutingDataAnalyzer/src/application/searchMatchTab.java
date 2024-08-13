819cb4320082faa8cb4f4a6e86cd82ce2e4d4d21
Arydragons <sunnybgp.icloud.com>
2024-08-13 13:28:38 -0500

    Made progress

--- ScoutingDataAnalyzer/src/application/searchMatchTab.java
+++ ScoutingDataAnalyzer/src/application/searchMatchTab.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.event.ActionEvent;
@@ -22,10 +23,12 @@ public class searchMatchTab extends GridPane implements EventHandler<ActionEvent
 	private ComboBox<String> isReplayCB;
 	private List<Integer> listOfTeamNums;
 	private MatchList mainML;
+	private TeamList theTL;
 	
-	public searchMatchTab(List<Integer> importedListOfTeamNums, boolean imIsPlayoff, MatchList imML) {
+	public searchMatchTab(List<Integer> importedListOfTeamNums, boolean imIsPlayoff, MatchList imML, TeamList imTeamList) {
 		listOfTeamNums = importedListOfTeamNums;
 		mainML = imML;
+		theTL = imTeamList;
 		if(imIsPlayoff == false) {
 			Label selTeamL = new Label("Select Team:  ");
 			Label selMatchNumL = new Label("Select Match Number:  ");
@@ -100,6 +103,7 @@ public class searchMatchTab extends GridPane implements EventHandler<ActionEvent
 	@Override
 	public void handle(ActionEvent event) {
 		try {
+			//Blue = false, Red = true
 			if(event.getSource() == submitNonB){
 				this.getChildren().clear();
 				String selTeamNumS = teamCB.getSelectionModel().getSelectedItem();
@@ -111,11 +115,139 @@ public class searchMatchTab extends GridPane implements EventHandler<ActionEvent
 				boolean isPrac = Boolean.parseBoolean(isPracS);
 				boolean isReplay = Boolean.parseBoolean(isReplayS);
 				List<Match> teamsMatch = mainML.getMatchFromFullList(matchNum);
-				List<Match> blueTeams;
-				List<Match> redTeams;
+				List<Match> blueTeams =  new ArrayList<>();
+				List<Match> redTeams =  new ArrayList<>();
+				TeamList redTs = new TeamList();
+				TeamList blueTs = new TeamList();
+				Boolean mainColor;
 				for(int i = 0; i < teamsMatch.size(); i++) {
-					
+					Match m = teamsMatch.get(i);
+					boolean mColor = m.getAllianceColor();
+					if(mColor == false) {
+						blueTeams.add(m);
+						blueTs.listOfTeams.add(theTL.getATeam(m.getMatchTeamNum()));
+						if(m.getMatchTeamNum() == selTeamNum) {
+							mainColor = false;
+						}
+					}else {
+						redTeams.add(m);
+						redTs.listOfTeams.add(theTL.getATeam(m.getMatchTeamNum()));
+						if(m.getMatchTeamNum() == selTeamNum) {
+							mainColor = true;
+						}
+					}
 				}
+				if(blueTeams.size() == 3) {
+					int teamNum1 = blueTeams.get(0).getMatchTeamNum();
+					int teamNum2 = blueTeams.get(1).getMatchTeamNum();
+					int teamNum3 = blueTeams.get(2).getMatchTeamNum();
+					Label blue1TeamNumL = new Label(Integer.toString(teamNum1));
+					Label blue2TeamNumL = new Label(Integer.toString(teamNum2));
+					Label blue3TeamNumL = new Label(Integer.toString(teamNum3));
+					blue1TeamNumL.setId("smallTitle");
+					blue2TeamNumL.setId("smallTitle");
+					blue3TeamNumL.setId("smallTitle");
+					Label blue1Cy = new Label(Integer.toString(blueTeams.get(0).getMatchAmp() + blueTeams.get(0).getMatchSpe() + blueTeams.get(0).getMatchTrap()));
+					Label blue2Cy = new Label(Integer.toString(blueTeams.get(1).getMatchAmp() + blueTeams.get(1).getMatchSpe() + blueTeams.get(1).getMatchTrap()));
+					Label blue3Cy = new Label(Integer.toString(blueTeams.get(2).getMatchAmp() + blueTeams.get(2).getMatchSpe() + blueTeams.get(2).getMatchTrap()));
+					blue1Cy.setId("avgStat");
+					blue2Cy.setId("avgStat");
+					blue3Cy.setId("avgStat");
+					Label blue1Amp = new Label(Integer.toString(blueTeams.get(0).getMatchAmp()));
+					Label blue2Amp = new Label(Integer.toString(blueTeams.get(1).getMatchAmp()));
+					Label blue3Amp = new Label(Integer.toString(blueTeams.get(2).getMatchAmp()));
+					Label blue1Spe = new Label(Integer.toString(blueTeams.get(0).getMatchSpe()));
+					Label blue2Spe = new Label(Integer.toString(blueTeams.get(1).getMatchSpe()));
+					Label blue3Spe = new Label(Integer.toString(blueTeams.get(2).getMatchSpe()));
+					Label blue1Trap = new Label(Integer.toString(blueTeams.get(0).getMatchTrap()));
+					Label blue2Trap = new Label(Integer.toString(blueTeams.get(1).getMatchTrap()));
+					Label blue3Trap = new Label(Integer.toString(blueTeams.get(2).getMatchTrap()));
+					blue1Amp.setId("avgStat");
+					blue2Amp.setId("avgStat");
+					blue3Amp.setId("avgStat");
+					blue1Spe.setId("avgStat");
+					blue2Spe.setId("avgStat");
+					blue3Spe.setId("avgStat");
+					blue1Trap.setId("avgStat");
+					blue2Trap.setId("avgStat");
+					blue3Trap.setId("avgStat");
+					Label blue1AvgCy = new Label(Double.toString(theTL.getATeam(teamNum1).getAvgCycles()));
+					Label blue2AvgCy = new Label(Double.toString(theTL.getATeam(teamNum2).getAvgCycles()));
+					Label blue3AvgCy = new Label(Double.toString(theTL.getATeam(teamNum3).getAvgCycles()));
+					Label blue1AvgSpe = new Label(Double.toString(theTL.getATeam(teamNum1).getAvgSpe()));
+					Label blue2AvgSpe = new Label(Double.toString(theTL.getATeam(teamNum2).getAvgSpe()));
+					Label blue3AvgSpe = new Label(Double.toString(theTL.getATeam(teamNum3).getAvgSpe()));
+					Label blue1AvgAmp = new Label(Double.toString(theTL.getATeam(teamNum1).getAvgAmp()));
+					Label blue2AvgAmp = new Label(Double.toString(theTL.getATeam(teamNum2).getAvgAmp()));
+					Label blue3AvgAmp = new Label(Double.toString(theTL.getATeam(teamNum3).getAvgAmp()));
+					Label blue1AvgTrap = new Label(Double.toString(theTL.getATeam(teamNum1).getAvgTrap()));
+					Label blue2AvgTrap = new Label(Double.toString(theTL.getATeam(teamNum2).getAvgTrap()));
+					Label blue3AvgTrap = new Label(Double.toString(theTL.getATeam(teamNum3).getAvgTrap()));
+					blue1AvgCy.setId("avgStat");
+					blue2AvgCy.setId("avgStat");
+					blue3AvgCy.setId("avgStat");
+					blue1AvgSpe.setId("avgStat");
+					blue2AvgSpe.setId("avgStat");
+					blue3AvgSpe.setId("avgStat");
+					blue1AvgAmp.setId("avgStat");
+					blue2AvgAmp.setId("avgStat");
+					blue3AvgAmp.setId("avgStat");
+					blue1AvgTrap.setId("avgStat");
+					blue2AvgTrap.setId("avgStat");
+					blue3AvgTrap.setId("avgStat");
+					Label blue1Cli = new Label(Integer.toString(blueTeams.get(0).getMatchClimb()));
+					Label blue2Cli = new Label(Integer.toString(blueTeams.get(1).getMatchClimb()));
+					Label blue3Cli = new Label(Integer.toString(blueTeams.get(2).getMatchClimb()));
+					Label blue1AvgCli = new Label(Double.toString(theTL.getATeam(teamNum1).getAvgClimb()));
+					Label blue2AvgCli = new Label(Double.toString(theTL.getATeam(teamNum2).getAvgClimb()));
+					Label blue3AvgCli = new Label(Double.toString(theTL.getATeam(teamNum3).getAvgClimb()));
+					blue1Cli.setId("avgStat");
+					blue2Cli.setId("avgStat");
+					blue3Cli.setId("avgStat");
+					blue1AvgCli.setId("avgStat");
+					blue2AvgCli.setId("avgStat");
+					blue3AvgCli.setId("avgStat");
+					Label blue1AvgCyQL = new Label("Avg Cycles");
+					Label blue1AvgSpeQL = new Label("Avg Speaker");
+					Label blue1AvgAmpQL = new Label("Avg Amp");
+					Label blue1AvgTrapQL = new Label("Avg Trap");
+					Label blue1AvgCliQL = new Label("Avg Climb");
+					Label blue2AvgCyQL = new Label("Avg Cycles");
+					Label blue2AvgSpeQL = new Label("Avg Speaker");
+					Label blue2AvgAmpQL = new Label("Avg Amp");
+					Label blue2AvgTrapQL = new Label("Avg Trap");
+					Label blue2AvgCliQL = new Label("Avg Climb");
+					Label blue3AvgCyQL = new Label("Avg Cycles");
+					Label blue3AvgSpeQL = new Label("Avg Speaker");
+					Label blue3AvgAmpQL = new Label("Avg Amp");
+					Label blue3AvgTrapQL = new Label("Avg Trap");
+					Label blue3AvgCliQL = new Label("Avg Climb");
+				}else if(blueTeams.size() == 2) {
+					int teamNum1 = blueTeams.get(0).getMatchTeamNum();
+					int teamNum2 = blueTeams.get(1).getMatchTeamNum();
+					Label blue1TeamNumL = new Label(Integer.toString(teamNum1));
+					Label blue2TeamNumL = new Label(Integer.toString(teamNum2));
+					Label noDataLabel2L = new Label("No Data");
+					blue1TeamNumL.setId("smallTitle");
+					blue2TeamNumL.setId("smallTitle");
+					noDataLabel2L.setId("smallTitle");
+				}else if(blueTeams.size() == 1) {
+					int teamNum1 = blueTeams.get(0).getMatchTeamNum();
+					Label blue1TeamNumL = new Label(Integer.toString(teamNum1));
+					Label noDataLabel1L = new Label("No Data");
+					Label noDataLabel2L = new Label("No Data");
+					blue1TeamNumL.setId("smallTitle");
+					noDataLabel1L.setId("smallTitle");
+					noDataLabel2L.setId("smallTitle");
+				}else if(blueTeams.size() == 0) {
+					Label noDataLabel1L = new Label("No Data");
+					Label noDataLabel2L = new Label("No Data");
+					Label noDataLabel3L = new Label("No Data");
+					noDataLabel1L.setId("smallTitle");
+					noDataLabel2L.setId("smallTitle");
+					noDataLabel3L.setId("smallTitle");
+				}
+
 			}
 			if(event.getSource() == submitPlayB) {
 				this.getChildren().clear();
